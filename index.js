e2c89e8efa9064e907310b5f68ea1666e08b9a6e
Muneeb <softmuneeb@gmail.com>
2023-08-22 13:39:39 +0500

    basic bot setup done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const TelegramBot = require('node-telegram-bot-api');
-const shortid = require('shortid'); // Assuming you have this module installed
+const shortid = require('shortid');
 
 dotenv.config();
 
@@ -33,21 +33,71 @@ async function start() {
 
     bot.onText(/\/start/, msg => {
       const chatId = msg.chat.id;
-      const greetings =
-        `Hello there! I'm your link-shortening bot. Here are the commands you can use:\n\n` +
-        `/start - Get a friendly greeting and command list\n` +
-        `/shorten [url] - Shorten a URL\n` +
-        `/mylinks - View your shortened links`;
+      const greetings = `Hello there! I'm your link-shortening bot. Here are the commands you can use`;
 
-      bot.sendMessage(chatId, greetings);
+      const keyboardOptions = {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: '/shorten', callback_data: 'shorten' }],
+            [{ text: '/mylinks', callback_data: 'mylinks' }],
+          ],
+        },
+      };
+
+      bot.sendMessage(chatId, greetings, keyboardOptions);
     });
 
-    bot.onText(/\/shorten (.+)/, async (msg, match) => {
+    bot.on('callback_query', async query => {
+      const chatId = query.message.chat.id;
+      const command = query.data;
+
+      switch (command) {
+        case 'shorten':
+          bot.sendMessage(
+            chatId,
+            'Please provide the URL you want to shorten.',
+          );
+          break;
+
+        case 'mylinks':
+          try {
+            const userLinks = await db
+              .collection('short_links')
+              .find({ chatId })
+              .toArray();
+
+            if (userLinks.length === 0) {
+              bot.sendMessage(chatId, 'No shortened links found.');
+            } else {
+              let response = 'Your shortened links:\n';
+              userLinks.forEach(link => {
+                response += `- ${link.originalUrl} -> ${link.shortUrl}\n\n`;
+              });
+              bot.sendMessage(chatId, response);
+            }
+          } catch (err) {
+            console.error('Error fetching user links:', err);
+            bot.sendMessage(
+              chatId,
+              'An error occurred while fetching your links.',
+            );
+          }
+          break;
+
+        default:
+          bot.sendMessage(chatId, 'Invalid command.');
+          break;
+      }
+
+      bot.answerCallbackQuery(query.id);
+    });
+
+    bot.onText(/^(?!\/)(.+)$/, async (msg, match) => {
       const chatId = msg.chat.id;
       const originalUrl = match[1];
 
       // Generate short URL logic
-      const shortUrl = shortid.generate();
+      const shortUrl = `https://custom-link.com/${shortid.generate()}`;
 
       // Store original and short URLs in MongoDB
       await db.collection('short_links').insertOne({
@@ -57,27 +107,6 @@ async function start() {
       });
 
       bot.sendMessage(chatId, `Shortened URL: ${shortUrl}`);
-    });
-
-    bot.onText(/\/mylinks/, async msg => {
-      const chatId = msg.chat.id;
-
-      const userLinks = await db
-        .collection('short_links')
-        .find({ chatId })
-        .toArray();
-
-      if (userLinks.length === 0) {
-        bot.sendMessage(chatId, 'No shortened links found.');
-        return;
-      }
-
-      let response = 'Your shortened links:\n';
-      userLinks.forEach(link => {
-        response += `${link.originalUrl} -> ${link.shortUrl}\n`;
-      });
-
-      bot.sendMessage(chatId, response);
     });
   } catch (err) {
     console.error('Error connecting to MongoDB:', err);
