40b188cc68027f5f7688a3fa19bb66bca4c2e412
Muneeb <softmuneeb@gmail.com>
2023-08-25 16:21:48 +0500

    Made and tested - woo hoo - buy a subscription - buy a domain - shorten a link - see subscriptions - see domains - see links

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const dotenv = require('dotenv');
+const shortid = require('shortid');
 dotenv.config();
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 
@@ -21,10 +22,14 @@ const options = {
 };
 
 const state = {};
+const linksOf = {};
+const domainsOf = {};
+const domainSold = {};
+const planEndingTime = {};
 
 bot.onText(/\/start/, msg => {
   const chatId = msg.chat.id;
-  state[chatId] = {};
+
   bot.sendMessage(
     chatId,
     'Welcome to the URL Shortener Bot! Please select an option:',
@@ -34,12 +39,30 @@ bot.onText(/\/start/, msg => {
 
 bot.onText(/Shorten a URL/, msg => {
   const chatId = msg.chat.id;
-  state[chatId].action = 'shorten';
+
+  // he must own a subscription
+  if (!isSubscribed(chatId)) {
+    bot.sendMessage(chatId, 'Subscribe to plans first');
+    return;
+  }
+  // he must own a domain name
+  if (!ownsDomainName(chatId)) {
+    bot.sendMessage(chatId, 'Buy a domain first');
+    return;
+  }
+
+  state[chatId].action = 'choose-domain';
   bot.sendMessage(chatId, 'Please provide the URL you want to shorten:');
 });
 
 bot.onText(/Buy a domain name/, msg => {
   const chatId = msg.chat.id;
+
+  // he must own a subscription
+  if (!isSubscribed(chatId)) {
+    bot.sendMessage(chatId, 'Subscribe to plans first');
+    return;
+  }
   state[chatId].action = 'buy';
   bot.sendMessage(chatId, 'Please enter the desired domain name:');
 });
@@ -63,7 +86,9 @@ bot.onText(/See my subscribed plan/, msg => {
   if (subscribedPlan) {
     bot.sendMessage(
       chatId,
-      `You are currently subscribed to the ${subscribedPlan} plan. Enjoy unlimited URL shortening with your purchased domain names.`,
+      `You are currently subscribed to the ${subscribedPlan} plan. Your plan is valid till ${new Date(
+        planEndingTime[chatId],
+      )}`,
     );
   } else {
     bot.sendMessage(chatId, 'You are not currently subscribed to any plan.');
@@ -93,12 +118,17 @@ bot.onText(/See my domains/, msg => {
     bot.sendMessage(chatId, 'You have no purchased domains yet.');
   }
 });
-
+const timeOf = {
+  Daily: 86400 * 1000,
+  Weekly: 7 * 86400 * 1000,
+  Monthly: 30 * 86400 * 1000,
+};
 bot.onText(/Daily|Weekly|Monthly/, (msg, match) => {
   const chatId = msg.chat.id;
   const plan = match[0];
   // Implement logic for handling subscription plans
   // For example, process payment and set subscription in the state
+  planEndingTime[chatId] = Date.now() + timeOf[plan];
   state[chatId].subscription = plan;
   bot.sendMessage(
     chatId,
@@ -110,12 +140,31 @@ bot.onText(/Daily|Weekly|Monthly/, (msg, match) => {
 bot.on('message', msg => {
   const chatId = msg.chat.id;
   const message = msg.text;
+  if (!state[chatId]) state[chatId] = {};
   const action = state[chatId]?.action;
 
-  if (action === 'shorten') {
+  if (action === 'choose-domain') {
+    const keyboard = [getPurchasedDomains(chatId)];
+    bot.sendMessage(
+      chatId,
+      `Please choose the domain you want to link with your short link`,
+      {
+        reply_markup: {
+          keyboard,
+          resize_keyboard: true,
+          one_time_keyboard: true,
+        },
+      },
+    );
+    state[chatId].action = 'shorten';
+    state[chatId].url = message;
+  } else if (action === 'shorten') {
     // Implement logic to shorten the provided URL
-    const shortenedURL = shortenURL(message); // Stubbed function
-    bot.sendMessage(chatId, `Your shortened URL is: ${shortenedURL}`);
+    const domain = message;
+    // delete state[chatId]?.selectedDomain;
+    const shortenedURL = shortenURLAndSave(chatId, domain, state[chatId].url);
+    bot.sendMessage(chatId, `Your shortened URL is: ${shortenedURL}`, options);
+    delete state[chatId]?.url;
     delete state[chatId]?.action;
   } else if (action === 'buy') {
     // Implement logic to check domain availability and process purchase
@@ -133,22 +182,43 @@ bot.on('message', msg => {
 
 // Stubbed functions for demonstration purposes
 function getShortenedLinks(chatId) {
-  return ['jamesten.com/hnbsGud']; // Replace with actual logic
+  console.log(linksOf[chatId]);
+  console.log(JSON.stringify(linksOf[chatId]));
+  return !linksOf[chatId]
+    ? []
+    : linksOf[chatId].map(d => `${d.shortenedURL} -> ${d.url}`); // Replace with actual logic
 }
 
 function getPurchasedDomains(chatId) {
-  return ['jamesten.com']; // Replace with actual logic
+  return domainsOf[chatId] || [];
 }
 
-function shortenURL(url) {
-  return 'SHORTENED_URL'; // Replace with actual logic
+function ownsDomainName(chatId) {
+  return domainsOf[chatId] && domainsOf[chatId].length > 0;
+}
+function isSubscribed(chatId) {
+  return planEndingTime[chatId] && planEndingTime[chatId] > Date.now();
+}
+
+// its not pure function // may need to refactor
+function shortenURLAndSave(chatId, domain, url) {
+  const shortenedURL = domain + '/' + shortid.generate();
+  const data = { url, shortenedURL };
+  linksOf[chatId] = linksOf[chatId] ? linksOf[chatId].concat(data) : [data];
+  return shortenedURL;
 }
 
 function buyDomain(chatId, domain) {
-  if (domain === 'jamesten.com') {
-    return 'Sorry, the domain name jamesten.com is already taken.';
+  if (domainSold[domain]) {
+    return `Sorry, the domain name ${domain} is already taken.`;
   }
+  domainSold[domain] = true;
+
+  // choose 1 format for domains like www.google.com, https://www.google.com, https://google.com, https://learn.google.com // Check new Url().domain etc or any other method to store domain names
   // Implement logic to process domain purchase
+  domainsOf[chatId] = domainsOf[chatId]
+    ? domainsOf[chatId].concat(domain)
+    : [domain];
   return (
     'Congratulations! You have successfully purchased the domain name ' + domain
   );
