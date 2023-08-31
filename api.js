c4bc3c06e17be718212b2ae65838696c05064d64
Muneeb <softmuneeb@gmail.com>
2023-08-31 13:11:14 +0500

    get req.hostname

--- api.js
+++ api.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Assalamo Alaikum' });
+  res.json({ message: 'Assalamo Alaikum', from: req.hostname });
 });
 
 const startServer = () => {
