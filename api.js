80117c7fe741a4d2695651707203f343d03a0814
Muneeb <softmuneeb@gmail.com>
2023-09-04 14:12:47 +0500

    get

--- api.js
+++ api.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
   res.json({ message: 'Assalamo Alaikum', from: req.hostname });
 });
 
-app.post('/save-payment-blockbee', (req, res) => {
+app.get('/save-payment-blockbee', (req, res) => {
   console.log('Full URL:', req.originalUrl);
   console.log('Received payment data:', req.body);
   // Here you can save the payment information to your database or perform any other necessary actions.
