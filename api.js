2036163dbbef2df2c2a62763e33697af693ce60f
Muneeb <softmuneeb@gmail.com>
2023-09-05 18:07:07 +0500

    util work, test https rest apis, json backup file of bot, json backup get endpoint

--- api.js
+++ api.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const fs = require('fs');
 
 const app = express();
 
 app.use(cors());
+
+app.set('json spaces', 2);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Assalamo Alaikum', from: req.hostname });
@@ -16,6 +19,18 @@ app.get('/save-payment-blockbee', (req, res) => {
   // bot send message: You payment is successful now you can buy domains and shorten links
   console.log(JSON.stringify(req.params, null, 2));
   res.json({ message: 'Payment data received and processed successfully' });
+});
+
+app.get('/get-json-data', (req, res) => {
+  fs.readFile('backup.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading JSON file:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    const jsonData = JSON.parse(data);
+    res.json(jsonData);
+  }); 
 });
 
 const startServer = () => {
