00d0b779c41cd3624a7f2bd17b1d59e47feb3182
Muneeb <softmuneeb@gmail.com>
2023-09-25 16:34:05 +0500

    log

--- startBot.js
+++ startBot.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const { convertUSDToCrypto } = require('./blockbee');
 const { isRailwayAPIWorking } = require('./cr-rl-connect-domain-to-server');
 const { getBusinessId } = require('./fincra');
@@ -5,10 +6,15 @@ const { getRegisteredDomainNames } = require('./get-purchased-domains.test');
 
 const runBot = async () => {
   try {
+    axios.get('https://api.ipify.org/').then(ip => {
+      const message = `Please add \`\`\`${ip.data}\`\`\` to whitelist in Connect Reseller, API Section. https://global.connectreseller.com/tools/profile`;
+      console.log(message);
+    });
+
     await getBusinessId();
     console.log('working, fincra api');
-    await getRegisteredDomainNames();
-    console.log('working, connect reseller api');
+    // await getRegisteredDomainNames();
+    // console.log('working, connect reseller api');
     await convertUSDToCrypto('1', 'polygon_matic');
     console.log('working, blockbee api');
     await isRailwayAPIWorking();
@@ -17,7 +23,13 @@ const runBot = async () => {
     require('./index.js');
   } catch (error) {
     console.error('Error is:', error);
-    console.error('Error is:', error?.message, error?.response?.data, error?.cause);
+    console.error(
+      'Error is:',
+      error?.message,
+      error?.response?.data,
+      error?.cause,
+      JSON.stringify(error?.response?.data, null, 2),
+    );
   }
 };
 
