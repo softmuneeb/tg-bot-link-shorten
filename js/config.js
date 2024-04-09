a1e0afee9fd09123ef9e9ba0e1e90b518bee0104
nomadcity <160823697+nomadcity@users.noreply.github.com>
2024-04-09 16:43:15 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -138,7 +138,7 @@ Nomadly Bot`,
 
   freeTrialNotAvailable: 'You have already used the free trial'
 
-  planSubscribed: `You have successfully subscribed to our {{plan}} plan. Enjoy our URL-shortening logics and Nomadly BulkSMS. Please download the app here: https://ap1s.net/6atvS. Need E-sim card? contact @Nomadlysupport',
+  planSubscribed: `You have successfully subscribed to our {{plan}} plan. Enjoy our URL-shortening logics and Nomadly BulkSMS. Please download the app here: https://ap1s.net/6atvS. Need E-sim card? contact @Nomadlysupport`,
 
   alreadySubscribedPlan: (days) => `Your subscription is active and expires in ${days}`,
 
