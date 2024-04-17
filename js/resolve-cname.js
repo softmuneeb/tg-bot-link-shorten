330c4a910b23f10b9c6d378b9a7a3761b65cfedd
softmuneeb@gmail.com <softmuneeb@gmail.com>
2024-04-17 17:42:55 +0500

    api code and schema updated for resolveDns CNAME

--- js/resolve-cname.js
+++ js/resolve-cname.js
@@ -3,9 +3,15 @@ const axios = require('axios')
 
 const resolveDns = async domain => {
   const response = await axios.get(`https://dns.google/resolve?name=${domain}&type=CNAME`)
-  const { Answer, Status, Comment } = response.data
+  const { Answer, Status, Comment, Authority } = response.data
   if (Status === 0 && Answer && Answer.length > 0) {
     const { name, TTL, data } = Answer[0]
+    log({ name, TTL, data })
+    return true
+  }
+
+  if (Status === 0 && Authority && Authority.length > 0) {
+    const { name, TTL, data } = Authority[0]
     log({ name, TTL, data })
     return true
   }
